d41eb0da2820420d1a3aceebae47bbbd1bccd6c1
Prem Sharma <sonusharma4237@gmail.com>
2025-06-05 19:15:24 +0530

    knapsack Memo added.

--- dp.java
+++ dp.java
@@ -55,6 +55,41 @@ public class dp {
         }
         return dp[n];
     }
+    // Knapsack Question -> By Recrsion method
+    public static int knapsack(int val[], int wt[], int w, int n){
+        if(w ==0 || n == 0){
+            return 0;
+        }
+        if(wt[n-1] <= w) { // valid condition
+            //include
+            int ans1 = val[n-1] + knapsack(val, wt, w-wt[n-1], n-1);
+            //exclude
+            int ans2 = knapsack(val, wt, w, n-1);
+            return Math.max(ans1, ans2);
+        }else{
+            return knapsack(val, wt, w, n-1);
+        }
+    }
+    // Knapsack By Memoization -> Time Comp: O(N)
+    public static int knapsackMemo(int val[], int wt[], int w, int n, int dp[][]){
+        if(w ==0 || n == 0){
+            return 0;
+        }
+        if(dp[n][w] != -1){
+            return dp[n][w];
+        }
+        if(wt[n-1] <= w) { // valid condition
+            //include
+            int ans1 = val[n-1] + knapsackMemo(val, wt, w-wt[n-1], n-1, dp);
+            //exclude
+            int ans2 = knapsackMemo(val, wt, w, n-1, dp);
+            dp[n][w] =  Math.max(ans1, ans2);
+            return dp[n][w];
+        }else{
+            dp[n][w] =  knapsackMemo(val, wt, w, n-1, dp);
+            return dp[n][w];
+        }
+    }
     public static void main(String[] args) {
         int n=5;
         int f[] = new int[n+1];
@@ -63,6 +98,17 @@ public class dp {
         int ways[ ] = new int[n+1];
         Arrays.fill(ways, -1);
         // System.out.println(climbwaysMemo(n, ways));
-        System.out.println(climbwaysTab(n));
+        // System.out.println(climbwaysTab(n));
+        int w=7;
+        int val[] = {15,14,10,45,30};
+        int wt[] ={2,5,1,3,4};
+        int dp[][] = new int[val.length+1][w+1];
+        for(int i=0; i<dp.length; i++){
+            for(int j=0; j<dp[0].length; j++){
+                dp[i][j]= -1;
+            }
+        }
+        //System.out.println(knapsack(val, wt, w, val.length));
+        System.out.println(knapsackMemo(val, wt, w, val.length, dp));
     }
 }
