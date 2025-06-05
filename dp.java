e4b3a1527df1a90714e9887e69fb64bc7243ea63
Prem Sharma <sonusharma4237@gmail.com>
2025-06-05 12:26:32 +0530

    Climb stair Tabu code addd .

--- dp.java
+++ dp.java
@@ -24,14 +24,14 @@ public class dp {
         }
         return f[n]; // Time Comp : O(N)
     }
-    // Climing Stairs 
+    // Climing Stairs -> Time Comp: Exponential -> O(2^N)
     public static int climbways(int n){
         if(n==0) return 1;
         if(n< 0) return 0;
 
         return climbways(n-1) + climbways(n-2);
     }
-    // ClimBing Starirs by Memoization ->
+    // ClimBing Starirs by Memoization -> Time Comp: O(N)
     public static int climbwaysMemo(int n , int ways[]){
         if(n==0) return 1;
         if(n< 0) return 0;
@@ -42,6 +42,19 @@ public class dp {
         ways[n] = climbwaysMemo(n-1, ways) + climbwaysMemo(n-2, ways);
         return ways[n];
     }
+    //Climbing Stairs By Tabulation -> Time Comp: O(N)
+    public static int climbwaysTab(int n){
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+        for(int i=1; i<=n; i++){
+            if(i == 1 ){
+                dp[i] = dp[i-1] +0;
+            }else{
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+        }
+        return dp[n];
+    }
     public static void main(String[] args) {
         int n=5;
         int f[] = new int[n+1];
@@ -49,6 +62,7 @@ public class dp {
         // System.out.println(climbways(5));
         int ways[ ] = new int[n+1];
         Arrays.fill(ways, -1);
-        System.out.println(climbwaysMemo(n, ways));
+        // System.out.println(climbwaysMemo(n, ways));
+        System.out.println(climbwaysTab(n));
     }
 }
