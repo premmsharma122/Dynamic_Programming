4daabb49c13247e4f68db9f4787432c26e3ffbe1
premm <sonusharma4237@gmail.com>
2025-06-27 01:19:59 +0530

    Update dp.java
    coin change code added

--- dp.java
+++ dp.java
@@ -127,6 +127,26 @@ public class dp {
 
     return dp[n][W];
 }
+    // Coin Change Question (Unbounded Knapsack)->
+public static int coinChange(int coins[], int sum, int dp[][]){
+    // Intilization
+    for(int i=0; i<coins.length+1; i++){
+        dp[i][0] = 1;
+    }
+    //code
+    for(int i=1; i<coins.length+1; i++){
+        for(int j=1; j<sum+1; j++){
+            if(coins[i-1]<=j){ // valid / include
+                dp[i][j] = dp[i][j-coins[i-1]] + dp[i-1][j];
+            }else{
+                dp[i][j] = dp[i-1][j];
+            }
+        }
+    }
+    return dp[coins.length][sum];
+
+}
+
 
     
     public static void main(String[] args) {
