8133dc3b4948fd777cfd9ca2f6b01f9d1eab2e57
premm <sonusharma4237@gmail.com>
2025-06-29 23:07:17 +0530

    Update dp.java

--- dp.java
+++ dp.java
@@ -146,9 +146,19 @@ public static int coinChange(int coins[], int sum, int dp[][]){
     return dp[coins.length][sum];
 
 }
-
-
-    
+     // Longest Comman Subsequence (Recursion Code)
+    public static int lcs(String s1, String s2, int n, int m){
+        if(n==0 || m == 0){
+            return 0;
+        }
+        if(s1.charAt(n-1) == s2.charAt(m-1)){
+            return lcs(s1,s2,n-1,m-1);
+        }else{
+            int ans1 = lcs(s1, s2, n-1, m);
+            int ans2 = lcs(s1, s2, n, m-1);
+            return Math.max(ans1,ans2);
+        }
+    }
     public static void main(String[] args) {
         int n=5;
         int f[] = new int[n+1];
