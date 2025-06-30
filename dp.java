ce1915655a4d8382cd54e6330a3a63be1a419427
premm <sonusharma4237@gmail.com>
2025-07-01 00:27:02 +0530

    Update dp.java

--- dp.java
+++ dp.java
@@ -186,7 +186,22 @@ public static int coinChange(int coins[], int sum, int dp[][]){
     }
     return dp[n][m];
 }
-
+    public int memolcss(String s1, String s2){
+        int n=s1.length(), m = s2.length();
+        int dp[][] = new int[n+1][m+1];
+        int ans=0;
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<m+1; j++){
+                if(s1.charAt(i-1) == s2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] +1;
+                    ans = Math.max(ans, dp[i][j]);
+                }else{
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         int n=5;
         int f[] = new int[n+1];
